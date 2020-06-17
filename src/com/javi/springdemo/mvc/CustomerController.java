16b5eb359b9a9c265d21201065d0d7a9606e428d
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-06-17 21:12:42 +0200

    Added Pre-processing Code with @InitBinder

--- src/com/javi/springdemo/mvc/CustomerController.java
+++ src/com/javi/springdemo/mvc/CustomerController.java
@@ -2,15 +2,31 @@ package com.javi.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an initBinder ... to convert trim input strings. Pre-process every String form data
+	// remove leading and trailing whitespace
+	// resolve issue for our validation. If String only has white space... trim it to null
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);//Defined in String API. true -> means trim to null.
+		//StringTrimmerEditor: removes whitespace - leading and trailing
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+		
+	}
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
