ed86a146e06a835647c8ac7598a14a6078d8def4
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-06-18 17:53:03 +0200

    added regular expression for postal code

--- src/com/javi/springdemo/mvc/Customer.java
+++ src/com/javi/springdemo/mvc/Customer.java
@@ -3,10 +3,12 @@ package com.javi.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
 	
+	//Variables
 	private String firstName;
 	
 	
@@ -17,6 +19,9 @@ public class Customer {
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")//regexp: Regular Expression. "^[a-zA-Z0-9]{5}": Characters and MetaCharacters
+	private String postalCode;
 	
 	//Getters and Setters
 	public String getFirstName() {
@@ -42,6 +47,15 @@ public class Customer {
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
 	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
 	
 	
 
